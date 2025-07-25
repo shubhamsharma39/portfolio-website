db9abfcd64e4d43c12bce1a9448233b924110461
Your Name <your-email@example.com>
2025-07-25 21:50:24 +0530

    Update Education section

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -11,17 +11,16 @@ const Education = () => {
         <div className="education__item">
           <h3>B.Tech in Computer Science</h3>
           <small>APJ Abdul Kalam Technical University | 2021 - 2025</small>
-          <p>CGPA: 7.5</p>
+          
         </div>
         <div className="education__item">
           <h3>12th (CBSE)</h3>
           <small>ST. Thomas International School  | 2019 - 2021</small>
-          <p>Percentage: 75%</p>
         </div>
         <div className="education__item">
           <h3>10th (CBSE)</h3>
           <small>Sant Keena Ram Seniour Secondary public School | 2017 - 2019</small>
-          <p>Percentage: 78%</p>
+         
         </div>
       </div>
     </section>
